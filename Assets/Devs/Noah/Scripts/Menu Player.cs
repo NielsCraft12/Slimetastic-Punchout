f52bbf69c7703e35aa894ea89db83ae92f48a28c
NielsCraft12 <nielsdelaat2@gmail.com>
2024-11-19 17:09:38 +0100

    ui werkt

--- Assets/Devs/Noah/Scripts/Menu Player.cs
+++ Assets/Devs/Noah/Scripts/Menu Player.cs
@@ -10,10 +10,10 @@ public class MenuPlayer : MonoBehaviour
 {
     private MenuManager menuManager;
     private RectTransform rectTransform;
+
     [SerializeField]
     private List<GameObject> PlayerMenuChilds = new List<GameObject>();
 
-    UIControls controls;
     private InputAction menu;
 
     [SerializeField]
@@ -21,77 +21,75 @@ public class MenuPlayer : MonoBehaviour
 
     private void Awake()
     {
-        controls = new UIControls();
         menuManager = GameObject.Find("Menu Manager").GetComponent<MenuManager>();
         rectTransform = GetComponent<RectTransform>();
 
-
         transform.SetParent(menuManager.playerSelections[0].transform, false);
-
 
         foreach (Transform playerMenuChilds in menuManager.playerSelections[0].transform)
         {
-            if (playerMenuChilds.gameObject.name == "Cosmetic Selection" || playerMenuChilds.gameObject.name == "Color Selection" || playerMenuChilds.gameObject.name == "Ready Selection")
+            if (
+                playerMenuChilds.gameObject.name == "Cosmetic Selection"
+                || playerMenuChilds.gameObject.name == "Color Selection"
+                || playerMenuChilds.gameObject.name == "Ready Selection"
+            )
             {
                 PlayerMenuChilds.Add(playerMenuChilds.gameObject);
             }
         }
 
-
         menuManager.playerSelections.RemoveAt(0);
-        rectTransform.localPosition = GameObject.Find("Cosmetic Selection").GetComponent<RectTransform>().localPosition;
-    }
-
-    private void OnEnable()
-    {
-        menu = controls.Menus.Move;
-        menu.Enable();
-        menu.performed += NavigateMenu;
-    }
-    private void OnDisable()
-    {
-        menu.Disable();
-    }
-
-    private void Update()
-    {
-     
-
+        rectTransform.localPosition = GameObject
+            .Find("Cosmetic Selection")
+            .GetComponent<RectTransform>()
+            .localPosition;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         collision.GetComponent<Image>().color = Color.red;
-        Debug.Log(collision.gameObject);
-
     }
+
     private void OnTriggerExit2D(Collider2D collision)
     {
         collision.GetComponent<Image>().color = Color.white;
     }
 
-    private void NavigateMenu(InputAction.CallbackContext context)
+    public void NavigateMenu(InputAction.CallbackContext context)
     {
-        Vector2 moveDir = menu.ReadValue<Vector2>();
+        Vector2 moveDir = context.ReadValue<Vector2>();
         Debug.Log(moveDir);
-
 
         if (moveDir == new Vector2(0, 1))
         {
-            selected--;
+            selected++;
         }
         else if (moveDir == new Vector2(0, -1))
         {
-            selected++;
+            selected--;
         }
-        if (selected > PlayerMenuChilds.Count -1)
+        else if (moveDir == new Vector2(-1, 0))
+        {
+            Debug.Log("left");
+        }
+        else if (moveDir == new Vector2(1, 0))
+        {
+            Debug.Log("Right");
+        }
+
+
+
+
+
+        if (selected > PlayerMenuChilds.Count - 1)
         {
             selected = 0;
-        } else if (selected < 0) {
-            selected = PlayerMenuChilds.Count -1;
+        }
+        else if (selected < 0)
+        {
+            selected = PlayerMenuChilds.Count - 1;
         }
 
         rectTransform.localPosition = PlayerMenuChilds[selected].transform.localPosition;
-
     }
 }
