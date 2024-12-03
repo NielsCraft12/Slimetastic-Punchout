81a97d78424ef0f94e822ebe300ea6ee46a3a325
NielsCraft12 <nielsdelaat2@gmail.com>
2024-12-03 23:13:54 +0100

    beetje meer sounds

--- Assets/Devs/Niels/Scripts/ButtonScript.cs
+++ Assets/Devs/Niels/Scripts/ButtonScript.cs
@@ -1,13 +1,37 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+using UnityEngine.UI; // Add this
 
-public class ButtonScript : MonoBehaviour
+public class ButtonScript : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler
 {
+    [SerializeField] private string hoverSoundName = "hoverSound";
+    [SerializeField] private string clickSoundName = "clickSound";
 
     private void Start()
     {
+        // Add click listener if this is a UI Button
+        Button button = GetComponent<Button>();
+        if (button != null)
+        {
+            button.onClick.AddListener(() => AudioManager.instance.Play(clickSoundName));
+        }
+
         AudioManager.instance.Play("BackgroundMusic");
     }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("Hover detected");
+        AudioManager.instance.Play(hoverSoundName);
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("Click detected");
+        AudioManager.instance.Play(clickSoundName);
+    }
+
     public void GoToScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
@@ -27,7 +51,5 @@ public class ButtonScript : MonoBehaviour
              Application.OpenURL("itch url ");
 #endif
     }
-
-
 
 }
